ac23484e4a113147e94377c8f15921a104a68f4c
Divyesh B Chudasama <divyesh@elastacloud.com>
2017-06-05 11:59:45 +0100

    First Commit

---
+++ MyJsonExtractor/MyJsonExtractor.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Analytics.Interfaces;
+using Microsoft.Analytics.Types.Sql;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+
+namespace MyJsonExtractor
+{
+	[SqlUserDefinedExtractor]
+
+	//Implementing interface IExtractor
+	public class MyJsonExtractor : IExtractor
+
+	{
+		private string pathOfRow;
+
+		//Constructor for MyJsonExtractor
+		public MyJsonExtractor(string pathOfRow = null)
+		{
+			this.pathOfRow = pathOfRow;
+			
+		}
+		
+		//Here overriding IEnumerable as we wish to use collections
+		public override IEnumerable<IRow> Extract(IUnstructuredReader input, IUpdatableRow output)
+		{
+
+			//The column deliminator
+			char col_delim = ',';
+			String line;
+
+			var reader = new StreamReader(input.BaseStream);
+			
+			while ((line = reader.ReadLine()) != null)
+			{
+				//Parse json a single token at a time
+				var token = line.Split(col_delim);
+				
+				//Iterating over Objects of type JObject. Objects are here represented as rows.
+				//foreach (JObject j in ChildObject(token, this.pathOfRow) )
+				{
+					// All fields are represented as columns
+					//this.RowConversion(o, output);
+				}
+				
+
+				yield return output.AsReadOnly();
+			}
+		}
+
+		private static IEnumerable<JObject> ChildObject(JToken root, string path)
+		{
+			// Path specified
+			if (!string.IsNullOrEmpty(path))
+			{
+				return root.SelectTokens(path).OfType<JObject>();
+			}
+
+			// Single JObject
+			var r = root as JObject;
+			if (r != null)
+			{
+				return new[] { r };
+			}
+
+			// Multiple JObjects
+			return root.Children().OfType<JObject>();
+		}
+
+
+	}
+
+}
---
+++ csvReader/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace csvReader
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			//Read in CSV
+			using (var reader = new StreamReader(File.OpenRead(@"C:\Users\gujra\OneDrive\Documents\Weather_Richard\Grid Disconnection Data for Hackfest2-3.csv")))
+
+			{
+				string[] lines;
+
+				//While loop until reading till the end of the csv file
+				while (!reader.EndOfStream)
+				{
+					//Read line by line
+					var line = reader.ReadLine();
+
+					//Here setting the line to split at a character, start a new line and remove any empty entries
+					lines = line.Split(new char[] { '\r' }, StringSplitOptions.RemoveEmptyEntries);
+
+					//Checking the number of rows
+					int rows = lines.Length;
+					//Checking the number of columns (First Row as this is the header file)
+					int col = lines[0].Split(',').Length;
+
+					//Data Array
+					string[] csvData = string[rows, col];
+
+					// Load the array.
+					for (int r = 0; r < rows; r++)
+					{
+						string[] line_r = lines[r].Split(',');
+						for (int c = 0; c < col; c++)
+						{
+							csvData[r, c] = line_r[c];
+						}
+					}
+
+					var sw = new StreamWriter();
+				}
+			}
+
+		}
+	}
+}
