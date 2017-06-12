4e9aaf353f8e6e4932eb6b550946d0f75a8157a3
Divyesh B Chudasama <divyesh@elastacloud.com>
2017-06-12 17:21:29 +0100

    Completed the code for extractor now debugging

--- MyJsonExtractor/MyJsonExtractor.cs
+++ MyJsonExtractor/MyJsonExtractor.cs
@@ -12,58 +12,74 @@ using Newtonsoft.Json.Linq;
 namespace MyJsonExtractor
 {
     //Implementing Interface IExtractor
-	public class CustomCSVExtractor : IExtractor
+    public class CustomCSVExtractor : IExtractor
     {
         //Overriding interface method 'Extract'
         public override IEnumerable<IRow> Extract(IUnstructuredReader input, IUpdatableRow output)
         {
-            IList<String> jsonObjectsAsString = new List<String>();
-            string jsonPath;
-            
+            IList<JObject> jsonObjectsList = new List<JObject>();
+            string jsonPath = null;
+     
+
             //Read in the JSON File using the JSONTextReader as provided by Newtonsoft.json Library
             using (JsonTextReader jtr = new JsonTextReader(new StreamReader(input.BaseStream)))
             {
                 //While the json reader reads to the end of the json file
                 while (jtr.Read())
                 {
-                        var jsonValue = jtr.Value;
-                        //Error Handling. Value pulls back the text value of the JToken in subject
-                        if (jsonValue == null)
+                    var jsonValue = jtr.Value;
+                    //Error Handling. Value pulls back the text value of the JToken in subject
+                    if (jsonValue == null)
+                    {
+                        //Missing Value
+                        Console.WriteLine("Unable to return the text value for:" + jsonValue + ". Are you missing a Json Value?");
+                    }
+                    else
+                    {
+
+                        //Creating a Json Token. (The Load method will achieve this for us)
+                        var jsonToken = JToken.Load(jtr);
+
+                        //Need to check to see if the user has specified a Path or not
+                        if (!string.IsNullOrEmpty(jsonPath))
                         {
-                            //Missing Value
-                            Console.WriteLine("Unable to return the text value for:" + jsonValue + ". Are you missing a Json Value?");
-                        }
-                        else
-                        {
+                            //Using the 'as' keyword to do a conversion from JToken to JObject
+                            var j = jsonToken as JObject;
 
-                            //Creating a Json Token. (The Load method will achieve this for us)
-                            var jsonToken = JToken.Load(jtr);
+                            //SelectTokens allows the user to query JSON using a single string path to a desired JToken. This makes dynamic queries really easy.
+                            //Using path query to allow us to extract child tokens in nested JSON Structures
+                            j.SelectTokens(jsonPath).OfType<JObject>();
 
-                            //SelectTokens allow s the user to query JSON using a single string path to a desired JToken. This makes dynamic queries really easy
-                            //jsonToken.SelectTokens(jsonPath).OfType<JObject>();
+                            //Children() will return a collection of child tokens of the specific token. Multiple Nested JObject Values 
+                            j.Children().OfType<JObject>();
 
-                            //Creating a new instance of JsonSerializer
-                            JsonSerializer js = new JsonSerializer();
+                            //Now adding the jsonObject to the JObject List
+                            jsonObjectsList.Add(j);
 
-                            //Deserialising json structure 
-                            string json = js.Deserialize<String>(jtr);
-
-                            //Now adding the deserialised json to a List
-                            jsonObjectsAsString.Add(json);
-
-                            //Iterate over each JSON Object
-                            foreach (String jsonStringObject in jsonObjectsAsString)
+                            //Iterate over each JSON Object in the list
+                            foreach (JObject p in jsonObjectsList)
                             {
-                                //Set this as the output as a Key, Value pair
-                                output.Set<string>(jsonObjectsAsString.Count, jsonStringObject);
+                                //Fetching the property 'name' of the JObject. This is the key. JProperty models a name-value pair contained within a JObject. JObjext is basically a JSON Object.
+                                //Each JObject has a collection of JProperties which are it's children, while each JProperty has a Name and a single child, it's value.
+                                foreach (JProperty jp in p.Properties())
+                                {
+                                    //Fetching the name property of the JSON Object
+                                    string name = jp.Name;
+                                    //Fetching the value of the JObject
+                                    JToken value = jp.Value;
+
+                                    //Set this as the output as a Key, Value pair
+                                    output.Set<object>(name,value);
+                                } 
                             }
                         }
+                    }
                 }
+
+                //Output as ReadOnly
+                yield return output.AsReadOnly();
             }
-
-            //Output as ReadOnly
-            yield return output.AsReadOnly();
         }
-    }
 
+    }
 }
--- jsonUnitTest/UnitTest1.cs
+++ jsonUnitTest/UnitTest1.cs
@@ -1,20 +1,22 @@
-﻿
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Newtonsoft.Json;
-using System.IO;
 using Newtonsoft.Json.Linq;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
 
-
-namespace testJson
+namespace jsonUnitTest
 {
-    class TestJSON
+    [TestClass]
+    public class UnitTest1
     {
-        static void Main(string[] args)
+        [TestMethod]
+        public void jsonTest()
         {
+            IList<JObject> jsonObjectsList = new List<JObject>();
+            string jsonPath = null;
+
             string json = @"
                             [{'airport':
                                 {'code':'ATL','name':'Atlanta, GA: Hartsfield-Jackson Atlanta International'},
@@ -28,26 +30,34 @@ namespace testJson
                               'EventProcessedUtcTime':'2017-05-04T15:08:34.1722594Z','PartitionId':0,'EventEnqueuedUtcTime':'2017-05-04T15:08:32.7370000Z'}
                             ]";
 
-            IList<JObject> jsonObjectsList = new List<JObject>();
-            //string jsonPath = null;
-
-
             using (JsonTextReader reader = new JsonTextReader(new StringReader(json)))
             {
                 while (reader.Read())
                 {
+                    if (reader.Value != null)
+                    {
+                        Console.WriteLine("Token: {0}, Value: {1}", reader.TokenType, reader.Value);
+                        Console.ReadLine();
+                    }
+                    else
+                    {
+                        Console.WriteLine("Token: {0}", reader.TokenType);
+                        Console.ReadLine();
+                    }
+
+                    /*
                     //Creating a Json Token. (The Load method will achieve this for us)
                     var jsonToken = JToken.Load(reader);
 
                     //Need to check to see if the user has specified a Path or not
-                    if (!string.IsNullOrEmpty(json))
+                    if (!string.IsNullOrEmpty(jsonPath))
                     {
                         //Using the 'as' keyword to do a conversion from JToken to JObject
                         var j = jsonToken as JObject;
 
                         //SelectTokens allows the user to query JSON using a single string path to a desired JToken. This makes dynamic queries really easy.
                         //Using path query to allow us to extract child tokens in nested JSON Structures
-                        j.SelectTokens(json).OfType<JObject>();
+                        j.SelectTokens(jsonPath).OfType<JObject>();
 
                         //Children() will return a collection of child tokens of the specific token. Multiple Nested JObject Values 
                         j.Children().OfType<JObject>();
@@ -55,23 +65,27 @@ namespace testJson
                         //Now adding the jsonObject to the JObject List
                         jsonObjectsList.Add(j);
 
+                        Console.WriteLine("JSON Object:", j);
+                        Console.ReadLine();
+                        
                         //Iterate over each JSON Object in the list
-                        foreach (JObject p in jsonObjectsList)
-                        {
-                            //Fetching the property 'name' of the JObject. This is the key. JProperty models a name-value pair contained within a JObject. JObjext is basically a JSON Object.
-                            //Each JObject has a collection of JProperties which are it's children, while each JProperty has a Name and a single child, it's value.
-                            foreach (JProperty jp in p.Properties())
-                            {
-                                //Fetching the name property of the JSON Object
-                                string name = jp.Name;
-                                //Fetching the value of the JObject
-                                JToken value = jp.Value;
-
-                                Console.WriteLine("Name: {0}, Value: {1}", name, value);
-                                Console.ReadLine();
-                            }
-                        }
+                        //foreach (JObject p in jsonObjectsList)
+                        //{
+                        //    //Fetching the property 'name' of the JObject. This is the key. JProperty models a name-value pair contained within a JObject. JObjext is basically a JSON Object.
+                        //    //Each JObject has a collection of JProperties which are it's children, while each JProperty has a Name and a single child, it's value.
+                        //    foreach (JProperty jp in p.Properties())
+                        //    {
+                        //        //Fetching the name property of the JSON Object
+                        //        string name = jp.Name;
+                        //        //Fetching the value of the JObject
+                        //        JToken value = jp.Value;
+                        //
+                        //        Console.WriteLine("Name: {0}, Value: {1}", name, value);
+                        //        Console.ReadLine();
+                        //    }
+                        //}
                     }
+                    */
                 }
             }
         }
